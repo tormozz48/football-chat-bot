4fb60a4e7b58e9bf3008fd8cbf9ae561caf5508e
tormozz48 <tormozz48@gmail.com>
2017-12-09 18:52:31 +0300

    Добавил конфигурационные файлы

---
+++ config/default.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    bot: {
+        token: ''
+    },
+    mongo: {
+        user: '',
+        password: '',
+        host: '127.0.0.1',
+        port: '27017',
+        database: 'football'
+    }
+};
---
+++ config/production.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    bot: {
+        token: '483957925:AAFKLlquecGE6c3KuoOiQu7DhyboRtssmjY'
+    },
+    mongo: {
+        user: 'football',
+        password: 'football',
+        host: 'ds135186.mlab.com',
+        port: '35186',
+        database: 'football'
+    }
+};

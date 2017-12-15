c89ac5c8f717fd2570be0f89ccd66a510dc1ee09
tormozz48 <tormozz48@gmail.com>
2017-12-15 18:38:22 +0300

    Подключил newrelic

---
+++ newrelic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.config = {
+    app_name: ['football-chat-bot'],
+    license_key: 'badfe55706eec5a278bf84ac60b254994c1d8e53',
+    logging: {
+        level: 'info'
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('newrelic');
+
 const path = require('path');
 const config = require('config');
 

b0ecba84eee034a518e8e9b74a3daeb7e1a44169
tormozz48 <tormozz48@gmail.com>
2018-01-27 21:39:28 +0300

    Добавил использование babel. Удалил лишние файлы

--- newrelic.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.config = {
-    app_name: ['football-chat-bot'],
-    license_key: 'badfe55706eec5a278bf84ac60b254994c1d8e53',
-    logging: {
-        level: 'info'
-    }
-};

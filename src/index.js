95e0ace5f09d4426c55651f5c5af088d5351fd90
tormozz48 <tormozz48@gmail.com>
2018-02-06 10:43:12 +0300

    Отключил ежедневные цитаты

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const config = require('config');
 const model = require('./model');
 const bot = require('./bot');
 const server = require('./server');
-const jobs = require('./jobs');
+//const jobs = require('./jobs');
 
 require('dotenv').config({path: path.join(process.cwd(), './config/config.env')});
 
@@ -19,12 +19,13 @@ const token = process.env.BOT_TOKEN || config.get('bot.token');
     try {
         const dbModel = await model.initialize();
 
-        const _bot = bot.create({token, model: dbModel});
+        // const _bot = bot.create({token, model: dbModel});
+        bot.create({token, model: dbModel});
 
         server.start();
 
         // jobs.serverPingJob(dbModel).start();
-        jobs.quoteJob(dbModel, _bot).start();
+        // jobs.quoteJob(dbModel, _bot).start();
     } catch (error) {
         console.error('Critical error. Process will be terminated');
         process.exit(1);

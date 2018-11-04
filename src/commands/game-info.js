7406fdf683cbbbee7e143afb983aad72b44c6ae5
andrey_kuznetsov48 <andrey.kuznetsov@kaspersky.com>
2018-11-04 22:07:37 +0300

    chore: add advanced debug messages

--- src/commands/game-info.js
+++ src/commands/game-info.js
@@ -3,12 +3,16 @@
 const answers = require('../../config/answers/commands/game-info');
 const utils = require('../utils');
 
+const debug = require('debug')('src:commands:game-info');
+
 module.exports = (model) => {
     const replier = utils.createReplier(answers);
 
     return async (ctx) => {
         const {id} = ctx.message.chat;
         let answer;
+
+        debug(`received command "/info" for chat ${id}`);
 
         try {
             const event = await model.Event.findOne({active: true, chat_id: id});
--- src/commands/help.js
+++ src/commands/help.js
@@ -3,10 +3,13 @@
 const answers = require('../../config/answers/commands/help');
 const utils = require('../utils');
 
+const debug = require('debug')('src:commands:help');
+
 module.exports = () => {
     const replier = utils.createReplier(answers);
 
     return async (ctx) => {
+        debug(`received command "/help"`);
         ctx.reply(replier.replySuccess()());
     };
 };
